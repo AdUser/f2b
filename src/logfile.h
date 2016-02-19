3024ba39e3fe485dabfb1fdee58f0f2a9d86f833
Alex 'AdUser' Z <ad_user@runbox.com>
2016-02-19 20:53:30 +1000

    + add prototypes in logfile.h

--- src/logfile.h
+++ src/logfile.h
@@ -11,4 +11,9 @@ typedef struct {
   struct stat st;
 } f2b_logfile_t;
 
+bool f2b_logfile_open(f2b_logfile_t *file, const char *filename);
+void f2b_logfile_close(const f2b_logfile_t *file);
+bool f2b_logfile_rotated(const f2b_logfile_t *file);
+bool f2b_logfile_getline(const f2b_logfile_t *file, char *buf, size_t bufsize);
+
 #endif /* F2B_LOGFILE_H_ */
