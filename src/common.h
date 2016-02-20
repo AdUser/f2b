76d4a47aace3213de4da96385147a4f0ab4d7718
Alex 'AdUser' Z <ad_user@runbox.com>
2016-02-20 15:28:01 +1000

    * common.h : more headers & macro FREE()

--- src/common.h
+++ src/common.h
@@ -3,9 +3,15 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#define FREE(x)   free(x), x = NULL
 
 #endif /* F2B_COMMON_H_ */
