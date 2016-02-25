b45a79bff9cb73b34c7ac2d332fccea4668c8b41
Alex 'AdUser' Z <ad_user@runbox.com>
2016-02-25 22:46:11 +1000

    * src/jail.h

--- src/jail.h
+++ src/jail.h
@@ -2,14 +2,18 @@
 #define F2B_JAIL_H_
 
 #include "logfile.h"
-#include "match.h"
+#include "ipaddr.h"
 
 #define LOGLINE_MAX 2048
 
 typedef struct f2b_jail_t {
   char name[32];
-  f2b_match_t   *matches;
+  time_t bantime;
+  size_t tries;
+  char glob[PATH_MAX];
   f2b_logfile_t *logfiles;
+  f2b_ipaddr_t  *pending;
+  f2b_ipaddr_t  *banned;
 };
 
 #endif /* F2B_JAIL_H_ */
