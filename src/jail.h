0472798e48cd6d6b748834db160055e2c55e2510
Alex 'AdUser' Z <ad_user@runbox.com>
2016-03-02 14:32:47 +1000

    * jail.h : updated f2b_jail_t

--- src/jail.h
+++ src/jail.h
@@ -1,19 +1,21 @@
 #ifndef F2B_JAIL_H_
 #define F2B_JAIL_H_
 
+#include "log.h"
 #include "logfile.h"
 #include "ipaddr.h"
-
-#define LOGLINE_MAX 2048
+#include "config.h"
 
 typedef struct f2b_jail_t {
-  char name[32];
+  bool enabled;
   time_t bantime;
   size_t tries;
+  char name[CONFIG_KEY_MAX];
+  char backend[CONFIG_KEY_MAX];
+  char backend_init[CONFIG_VAL_MAX];
   char glob[PATH_MAX];
   f2b_logfile_t *logfiles;
-  f2b_ipaddr_t  *pending;
-  f2b_ipaddr_t  *banned;
-};
+  f2b_ipaddr_t  *ipaddrs;
+} f2b_jail_t;
 
 #endif /* F2B_JAIL_H_ */
