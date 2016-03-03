f9662ace7e31058a305d0a0aec2db6a4a1f548f0
Alex 'AdUser' Z <ad_user@runbox.com>
2016-03-03 18:21:59 +1000

    * update jail definition

--- src/jail.h
+++ src/jail.h
@@ -5,17 +5,19 @@
 #include "logfile.h"
 #include "ipaddr.h"
 #include "config.h"
+#include "regexps.h"
+#include "backend.h"
 
 typedef struct f2b_jail_t {
   bool enabled;
   time_t bantime;
   size_t tries;
   char name[CONFIG_KEY_MAX];
-  char backend[CONFIG_KEY_MAX];
-  char backend_init[CONFIG_VAL_MAX];
   char glob[PATH_MAX];
   f2b_logfile_t *logfiles;
+  f2b_regex_t   *regexps;
   f2b_ipaddr_t  *ipaddrs;
+  f2b_backend_t *backend;
 } f2b_jail_t;
 
 #endif /* F2B_JAIL_H_ */
