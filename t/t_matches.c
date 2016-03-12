2f26e6bf1bc7929ce9c0b57be873551d8caee143
Alex 'AdUser' Z <ad_user@runbox.com>
2016-03-12 22:48:46 +1000

    * fix test in release mode

--- t/t_matches.c
+++ t/t_matches.c
@@ -3,18 +3,25 @@
 
 int main() {
   f2b_matches_t matches;
+  bool result = false;
   size_t size = 15;
   time_t now  = time(NULL);
 
-  assert(f2b_matches_create(&matches, size) == true);
+  UNUSED(result);
+
+  result = f2b_matches_create(&matches, size);
+  assert(result == true);
   assert(matches.used == 0);
   assert(matches.max == 15);
   assert(matches.times != NULL);
 
-  for (size_t i = 0; i < size; i++)
-    assert(f2b_matches_append(&matches, now - 60 * i) == true);
+  for (size_t i = 0; i < size; i++) {
+    result = f2b_matches_append(&matches, now - 60 * i);
+    assert(result == true);
+  }
 
-  assert(f2b_matches_append(&matches, 0) == false);
+  result = f2b_matches_append(&matches, 0);
+  assert(result == false);
 
   f2b_matches_expire(&matches, now - 60 * 4);
   assert(matches.used == 4);
