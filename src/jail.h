8fdabf2b9d5c487734d8ef3c241334e106e1969c
Alex 'AdUser' Z <ad_user@runbox.com>
2016-03-15 13:51:19 +1000

    * apply license

--- src/jail.h
+++ src/jail.h
@@ -1,3 +1,9 @@
+/* Copyright 2016 Alex 'AdUser' Z (ad_user@runbox.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 #ifndef F2B_JAIL_H_
 #define F2B_JAIL_H_
 
--- src/logfile.c
+++ src/logfile.c
@@ -1,3 +1,9 @@
+/* Copyright 2016 Alex 'AdUser' Z (ad_user@runbox.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 #include "common.h"
 #include "logfile.h"
 
