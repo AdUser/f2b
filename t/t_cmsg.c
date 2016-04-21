7ebc8c7bd20ddcf9b5f82c4486039a849f7dfce6
Alex 'AdUser' Z <ad_user@runbox.com>
2016-04-21 17:07:23 +1000

    + t/t_cmsg.c

---
+++ t/t_cmsg.c
@@ -0,0 +1,24 @@
+#include "../src/common.h"
+#include "../src/cmsg.h"
+
+int main() {
+  const char *argv[DATA_ARGS_MAX];
+  f2b_cmsg_t msg;
+
+  memset(&msg, 0x0, sizeof(msg));
+  memset(argv, 0x0, sizeof(argv));
+
+  snprintf(msg.data, sizeof(msg.data), "test1\ntest2\n");
+  msg.size = strlen(msg.data);
+
+  f2b_cmsg_convert_args(&msg);
+  assert(memcmp(msg.data, "test1\0test2\0", 12) == 0);
+
+  f2b_cmsg_extract_args(&msg, argv);
+  assert(argv[0] == msg.data + 0);
+  assert(argv[1] == msg.data + 6);
+  assert(memcmp(argv[0], "test1\0", 6) == 0);
+  assert(memcmp(argv[1], "test2\0", 6) == 0);
+
+  return EXIT_SUCCESS;
+}
