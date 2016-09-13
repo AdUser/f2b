c26b527fb34c5ea37d716b4acbc149bd023a16b2
Alex 'AdUser' Z <ad_user@runbox.com>
2016-09-13 17:47:09 +1000

    * src/sources/source.h : more includes

--- src/sources/source.h
+++ src/sources/source.h
@@ -4,7 +4,14 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+#include <assert.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../strlcpy.h"
 
 #define INIT_MAX 256
 
